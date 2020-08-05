1d6be877f7327a6624e837f5fc584ab80739f3e5
nick wuerz <nawuerz@gmail.com>
2020-08-05 18:13:39 -0500

    added send alert to contact form

--- public/contact.js
+++ public/contact.js
@@ -1,12 +1,28 @@
-$("#submitBtn").on("click", ()=>{
-    const newMessage = {
-        from: $("#email").val().trim(),
-        subject: $("#name").val().trim(),
-        text: $("#message").val().trim()
-    };
+$(document).ready(function () {
+  const formAlert = $("#form-alert");
+  const submitButton = $("#submitBtn");
+  const message = $("#message");
+  const email = $("#email");
+  const name = $("#name");
 
-    $.post("/mail", newMessage)
-    .then((data)=> {
-        console.log(data);
+  formAlert.hide();
+  submitButton.on("click", (event) => {
+    // alert that message was sent //
+    event.preventDefault();
+    const newMessage = {
+      from: email.val().trim(),
+      subject: name.val().trim(),
+      text: message.val().trim(),
+    };
+    $.post("/mail", newMessage).then((data) => {
+      console.log(data);
+      email.val("");
+      name.val("");
+      message.val("");
+      $("#form-alert").show();
+      setTimeout(function () {
+        $("#form-alert").hide();
+      }, 2000);
     });
+  });
 });
